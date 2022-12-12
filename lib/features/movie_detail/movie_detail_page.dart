d147bf7902f6c5b6770608ce7d9f8ce5442d7c49
Nicholas 关宏庚 <nicholas.05apr@gmail.com>
2022-12-12 22:00:19 +0700

    Change text color on scroll + add network client on entitlements

--- lib/features/movie_detail/movie_detail_page.dart
+++ lib/features/movie_detail/movie_detail_page.dart
@@ -10,30 +10,60 @@ class MovieDetailPage extends StatefulWidget {
 }
 
 class _MovieDetailPageState extends State<MovieDetailPage> {
+  double _scrollColorLerp = 0.0;
+
   @override
   Widget build(BuildContext context) {
     final appTheme = Theme.of(context);
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            stretch: true,
-            expandedHeight: 200,
-            // title: Text('App bar is here'),
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title: Text(
-                'Movie title goes here',
-                style: TextStyle(color: appTheme.colorScheme.inverseSurface),
-              ),
-              background: Image.network(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
-                fit: BoxFit.cover,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 200,
+              iconTheme: IconThemeData(
+                  color: Color.lerp(appTheme.colorScheme.onSurfaceVariant,
+                      Colors.white, _scrollColorLerp)),
+              // title: Text('App bar is here'),
+              flexibleSpace: LayoutBuilder(
+                builder: (context, constraints) {
+                  double titleBarConstraint = constraints.biggest.height;
+                  double safeAreaTop = MediaQuery.of(context).padding.top;
+                  // Pakai angka ini untuk bahan pertimbangan warna text
+                  print(
+                      'Title Bar Height: ${titleBarConstraint - safeAreaTop}');
+                  // 56 - 110 (title bar value)
+                  // 0 - 1 (lerp value)
+                  double titleBarHeight = titleBarConstraint - safeAreaTop;
+
+                  if (titleBarHeight <= 110) {
+                    _scrollColorLerp = (titleBarHeight - 56) / (110 - 56);
+                  } else {
+                    _scrollColorLerp = 1;
+                  }
+                  return FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text(
+                      'Movie title goes here',
+                      style: TextStyle(
+                          color: Color.lerp(appTheme.colorScheme.inverseSurface,
+                              Colors.white, _scrollColorLerp)),
+                    ),
+                    background: Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
               ),
             ),
-          ),
-        ],
+          ];
+        },
+        body: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context, index) => Text("Item $index"),
+        ),
       ),
     );
   }
